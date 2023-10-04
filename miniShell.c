67136214b90d31d3f95fa6f4e55fd27f1132b4d3
gitsm0414 <tjtjdals414@gmail.com>
2023-10-05 00:36:30 +0900

    second commit

--- miniShell.c
+++ miniShell.c
@@ -8,7 +8,7 @@
 char* cmd=NULL;  //pointer for command string, global
 char** args;  //pointer array for each arguments in command, global
 
-void cleanup(void){
+void cleanup(void){ //free global malloc variables
 	free(cmd);
 	for(int i=0; i<ARGSIZE; i++)
 		free(args[i]);
@@ -20,23 +20,32 @@ int main(){
 	char path[100];
 
 	size_t size;
-	ssize_t cmdread;//related to cmd reading error
-	
+	ssize_t cmdread; //related to cmd reading error
+	pid_t pid;
+
 	int i;
-	int cmpresult;//related to 'quit'
-	
+	int cmp; //related to 'quit'
+
 	atexit(cleanup);
 
 	size = 100;
 	arg = (char**)malloc(sizeof(char*) * ARGSIZE);
 
-	do{	//getline error handling
-		if((cmdread = getline(&cmd, &size, stdin)) == -1) exit(1);////
+	do{
+		//getline error handling
+		if((cmdread = getline(&cmd, &size, stdin)) == -1) 
+			perror("command reading error"), exit(1);
 		cmd[strlen(cmd)-1] = '\0';
 
 		//quit
-		if((cmpresult = strcmp("quit",cmd))==0) exit(1);////
-		
+		if((cmp = strcmp("quit",cmd))==0){
+			//if parent has no child, then wait() returns -1 right away
+			//parent waits all possible child processes then quit
+			while(true){
+				if((pid = wait(NULL)) == -1) break;
+			}		
+			exit(0);
+		}	
 		//cmd parsing -> args
 		i = 0;
 		ptr = strtok(cmd, " ");
@@ -49,18 +58,24 @@ int main(){
 		//path
 		sprintf(path, "/bin/%s", args[0]);
 		
-
+		//if child, then execute the cmd
 		if(fork() == 0){
 			int exe;
 			if((exe = execv(path, args)) == -1){
+				perror("command executing error");
 				exit(1);
-			}
-			
+			}else{
+				exit(0);
+			}	
 		}
-		wait(NULL); ////
+		else //if parent, wait until child process ends
+		{ 
+			wait(NULL);
+		}	
+			 
 				
 
-	}while(1);
+	}while(true);
 	
 	
 
